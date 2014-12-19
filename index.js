148c9bcb5571a352aa0c5e3e88e67e29330e3dd9
Yury Chebotaev <yury.79120345101@gmail.com>
2014-12-20 04:27:01 +0500

    add ready hook

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 function FakeConnection(promise, callback){
   var self = this;
   self.connection = null;
-  self.promise = promise.cancellable().then(function(connection){
-    self.connection = connection;
-    return self;
-  }, function(err){
-    self.promise.cancel(err);
-  }).nodeify(callback);
+  self.Promise = promise.constructor;
+  self.promise = promise
+    .cancellable()
+    .nodeify(function(err, connection){
+      var deferred = self.Promise.defer();
+      if (err == null && typeof callback === 'function'){
+        if (callback.length >= 3){
+          callback.call(self, err, connection, function(err){
+            if (err != null) deferred.reject(err);
+            deferred.resolve(connection);
+          });
+        } else {
+          promise = callback.call(self, err, connection);
+          if (promise != null && typeof promise.then === 'function'){
+            promise.then(function(){
+              deferred.resolve(connection);
+            }, function(err){
+              deferred.reject(err);
+            });
+          } else {
+            deferred.resolve(connection);
+          }
+        }
+      } else 
+      if (err != null) {
+        deferred.reject(err);
+      } else {
+        deferred.resolve(connection);
+      }
+      return deferred.promise;
+    })
+    .then(function(connection){
+      return self.connection = connection;
+    }, function(err){
+      self.promise.cancel(err);
+    });
 }
 
 FakeConnection.prototype._start = function(term, cb, options){
@@ -14,10 +44,18 @@ FakeConnection.prototype._start = function(term, cb, options){
   if (self.connection == null){
     self.promise.then(function(connection){
       connection._start(term, cb, options);
-    });
+    }, cb);
   } else {
     self.connection._start(term, cb, options);
   }
+}
+
+FakeConnection.prototype.then = function(resolved, rejected){
+  return this.promise.then(function(connection){
+    resolved(connection);
+  }, function(error){
+    rejected(error);
+  });
 }
 
 exports.patch = function(r){
--- readme.md
+++ readme.md
@@ -26,6 +26,47 @@ r.dbList().run(fakeConnection, function(err, dbList){
 });
 ```
 
+FakeConnection#then
+-------------------
+
+there is also `.then` helper method that proxied to a `.promise.then` method call
+
+```javascript
+fakeConnection.then(function(connection){
+  return r.dbList().run(connection);
+});
+```
+
+all `.then` callbacks will be called only after actual `.connection` arrived
+
+connection ready hook
+---------------------
+
+since `r.connect` is now always returs "valid" connection, there is special meaning for `r.connect(function(err, connection))` callback.
+
+this callback is now have **before all other requests** meaning. this means that this callback is guaranteed to invoked before any other database requests once connection it availible
+
+**note** that `err` argument in callback is always `null` because if there will be actual error, `.promise` will be rejected with this error
+
+`r.connect` method is now overloaded as following:
+
+```function
+r.connect(function(err, connection){
+  return "any non-promise value has no effect";
+});
+
+r.connect(function(err, connection){
+  // Promises are counts
+  return r.dbCreate('foo').run(connection);
+});
+
+r.connect(function(err, connection, done){
+  r.dbCreate('foo').run(connection, done);
+});
+```
+
+**note** that you cannot use `done` with Promise. you only able to return promise from this callback if you have less than 3 parameters in function signature. besides if you have `done` parameter in function signature, you cannot return Promise from this callback
+
 caveats
 -------
 
